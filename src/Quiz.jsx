b1b2d81baf7676f6735baeb8cf77b863b5fc8c1e
Anjusha1141 <anjusha1141@gmail.com>
2024-06-14 11:10:32 +0530

    Quiz App

---
+++ src/Quiz.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div>
+        <p>Question</p>
+        
+        <button type='button'>
+            option1
+        </button>
+
+    </div>
+  )
+}
+
+export default Quiz
---
+++ src/quizpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const quizpage = () => {
+  return (
+    <div>
+        <p>Question</p>
+        <button type='button'>
+            option1
+        </button>
+    </div>
+  )
+}
+
+export default quizpage
